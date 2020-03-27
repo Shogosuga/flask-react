bc71e6e1807032a261f133018e248580e9992b65
Shogosuga <shogo1219tdj@gmail.com>
2020-03-27 16:24:30 +0900

    mod

--- static/js/index.js
+++ static/js/index.js
@@ -19,6 +19,54 @@ class LikeButton extends React.Component {
   }
 }
 
+class TitleBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value : 'タイトル' };
+        this.handleChange = this.handleChange.bind(this)
+    };
+
+    handleChange (title) {
+        this.setState({ value : title.target.value })
+    };
+
+    render() {
+        //return (
+        //    <div>
+        //        <p>
+        //            { this.state.value }
+        //        </p>
+        //        <input value={ this.state.value } onChange={ this.handleChange } />
+        //    </div>
+        //)
+        return e(
+            'div',
+            null,
+            e(
+                'p',
+                null,
+                this.state.value
+            ),
+            e(
+                'input',
+                {
+                    value : this.state.value,
+                    onChange : this.handleChange
+                }
+            )
+        )
+    };
+};
+
 
 const domContainer = document.querySelector('#root');
 ReactDOM.render(e(LikeButton), domContainer);
+
+//ReactDOM.render(
+//    <TitleBox />,
+//    document.getElementsByClassName('editor-titlebox__title')
+//)
+ReactDOM.render(
+    e(TitleBox),
+    document.getElementById('editor-titlebox__title')
+)
